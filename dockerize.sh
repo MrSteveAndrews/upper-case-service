b535ee856740df3e6cf06fff3fb1864a73c24117
MrSteveAndrews <sandrews@blue-agility.com>
2016-09-12 16:42:09 -0700

    Added sudo to script.

--- dockerize.sh
+++ dockerize.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #Build and tag the image using the Dockerfile.  Pass in a tag as a parameter.  The CD server will pass in the pipeline count.
-docker build -t blueagility/uppercase-service:$1 -t blueagility/uppercase-service:latest .
+sudo docker build -t blueagility/uppercase-service:$1 .
+sudo docker build -t blueagility/uppercase-service:latest .
 
 # Push the image to DockerHub
-docker login -u $2 -p $3
-docker push blueagility/uppercase-service:$1
-docker push blueagility/uppercase-service:latest
+sudo docker login -u $2 -p $3
+sudo docker push blueagility/uppercase-service:$1
+sudo docker push blueagility/uppercase-service:latest
