b5e402ef01cf0fbf7417dc29225c99ce5fe8eadc
MrSteveAndrews <sandrews@blue-agility.com>
2016-09-16 22:10:32 -0700

    Tag artifact as acceptance test candidate.  Force re-label

--- dockerize.sh
+++ dockerize.sh
@@ -3,13 +3,13 @@
 #Build and tag the image using the Dockerfile.  Pass in a tag as a parameter.  The CD server will pass in the pipeline count.
 echo docker build -t blueagility/uppercase-service:$1 .
 docker build -t blueagility/uppercase-service:$1 .
-echo docker tag blueagility/uppercase-service:$1 blueagility/uppercase-service:latest
-docker tag blueagility/uppercase-service:$1 blueagility/uppercase-service:latest
+echo docker tag -f blueagility/uppercase-service:$1 blueagility/uppercase-service:at-candidate
+docker tag blueagility/uppercase-service:$1 blueagility/uppercase-service:at-candidate
 
 # Push the image to DockerHub
 echo docker login -u $2 -p $3
 docker login -u $2 -p $3
 echo docker push blueagility/uppercase-service:$1
 docker push blueagility/uppercase-service:$1
-echo docker push blueagility/uppercase-service:latest
-docker push blueagility/uppercase-service:latest
+echo docker push blueagility/uppercase-service:at-candidate
+docker push blueagility/uppercase-service:at-candidate
